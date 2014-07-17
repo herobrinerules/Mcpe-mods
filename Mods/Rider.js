693e1f16771b276205422a0c7f6f18a030f1b1cd
feehi <feehi@users.noreply.github.com>
2014-07-17 23:31:53 +0700

    Updated Rider.js

--- Mods/Rider.js
+++ Mods/Rider.js
@@ -1,3 +1,10 @@
-## A New Post
+/*Ride Everything Mod By FeeTheAwesomest*/
 
-Enter text in [Markdown](http://daringfireball.net/projects/markdown/). Use the toolbar above, or click the **?** button for formatting help.
+ModPe.addCraftRecipe(464, 1, 0, [334, 3, 0, 287, 2, 0]);
+ModPe.setItem(464, "record_cat", 0, "Make Ride On You tool");
+Item.setItemCategory(464,ItemCategory.TOOL);
+ModPe.addCraftRecipe(463, 1, 0, [334, 3, 0, 287, 2, 0]);
+ModPe.setItem(463, "record_blocks", 0, "Ride On tool");
+Item.setItemCategory(463,ItemCategory.TOOL);
+var items_opaque = "http://i.imgur.com/w5AHbXE.png";
+ModPE.overrideTexture("images/items-opaque.png", items_opaque);
