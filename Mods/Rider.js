6274974b4af00835cc8a5c4112b86192fb0c44b7
feehi <feehi@users.noreply.github.com>
2014-07-18 23:20:24 +0700

    Updated Rider.js

--- Mods/Rider.js
+++ Mods/Rider.js
@@ -1,4 +1,15 @@
 /*Ride Everything Mod By FeeTheAwesomest*/
 
+//Crafting for saddles
 ModPe.addCraftRecipe(329, 1, 0, [334, 3, 0, 287, 2, 0]);
 Item.setItemCategory(329,ItemCategory.TOOL);
+
+//Hit a mob with a saddle to ride it
+function attackHook(a, v) {
+    if (a == Player.getEntity()) {
+        if (329 == getCarriedItem()) {
+          function rideAnimal(a,v);
+          
+        }
+    }
+}
