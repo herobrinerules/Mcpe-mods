e549288fc8aaff989cb25ec9ce5577992074ead4
feehi <feehi@users.noreply.github.com>
2014-07-18 23:34:30 +0700

    Updated Rider.js

--- Mods/Rider.js
+++ Mods/Rider.js
@@ -1,5 +1,7 @@
 /*Ride Everything Mod By FeeTheAwesomest*/
 
+var item=Player.getCarriedItem();
+
 //Crafting for saddles
 ModPe.addCraftRecipe(329, 1, 0, [334, 3, 0, 287, 2, 0]);
 Item.setItemCategory(329,ItemCategory.TOOL);
@@ -9,7 +11,15 @@ function attackHook(a, v) {
     if (a == Player.getEntity()) {
         if (329 == getCarriedItem()) {
           function rideAnimal(a,v);
-          
+        }
+    }
+}
+
+//Hit a mob with any type of shovel
+function attackHook(a, v) {
+    if (a == Player.getEntity()) {
+      if(item()==269||item()==274||item()==256||item()==284||item()==277) {
+        function rideAnimal(v,a);
         }
     }
 }
