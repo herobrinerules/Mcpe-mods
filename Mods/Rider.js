dbe874ad47422d5e8b779c1ba0cb197bdc885367
feehi <feehi@users.noreply.github.com>
2014-07-18 17:21:16 +0700

    Update Rider.js

--- Mods/Rider.js
+++ Mods/Rider.js
@@ -1,10 +1,4 @@
 /*Ride Everything Mod By FeeTheAwesomest*/
 
-ModPe.addCraftRecipe(464, 1, 0, [334, 3, 0, 287, 2, 0]);
-ModPe.setItem(464, "record_cat", 0, "Make Ride On You tool");
-Item.setItemCategory(464,ItemCategory.TOOL);
-ModPe.addCraftRecipe(463, 1, 0, [334, 3, 0, 287, 2, 0]);
-ModPe.setItem(463, "record_blocks", 0, "Ride On tool");
-Item.setItemCategory(463,ItemCategory.TOOL);
-var items_opaque = "http://i.imgur.com/w5AHbXE.png";
-ModPE.overrideTexture("images/items-opaque.png", items_opaque);
+ModPe.addCraftRecipe(329, 1, 0, [334, 3, 0, 287, 2, 0]);
+Item.setItemCategory(329,ItemCategory.TOOL);
