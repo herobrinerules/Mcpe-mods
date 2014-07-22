4f07bc87296b3d716b98825d8eac5e1a297a1ab1
feehi <feehi@users.noreply.github.com>
2014-07-22 21:59:42 +0700

    Updated Rider.js

--- Mods/Rider.js
+++ Mods/Rider.js
@@ -9,16 +9,16 @@ Item.setItemCategory(329,ItemCategory.TOOL);
 //Hit a mob with a saddle to ride it
 function attackHook(a, v) {
     if (a == Player.getEntity()) {
-        if (329 == getCarriedItem()) {
+        if (item=329) {
           function rideAnimal(a,v);
         }
     }
 }
 
-//Hit a mob with any type of shovel
+//Hit a mob with any type of shovel to ride it
 function attackHook(a, v) {
     if (a == Player.getEntity()) {
-      if(item()==269||item()==274||item()==256||item()==284||item()==277) {
+      if(item==269||item==274||item==256||item==284||item==277) {
         function rideAnimal(v,a);
         }
     }
